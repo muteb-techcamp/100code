0d7eaa38f40cc3014789f5527041093af7d6ec0a
mutebalotaibi <mzmz_222@hotmail.com>
2019-08-24 19:27:59 +0300

    day5 task

---
+++ day5.py
@@ -0,0 +1,12 @@
+'''اليوم الخامس
+المطلوب طباعة متغير من خلال حاصل جمع عدد من النصوص وذلك بعد فصلها 
+ using split & len function to print the varibles from type string 
+ '''
+ 
+x='apple'
+y='orange'
+z='limon'
+basket=x+' '+ y+' '+ z
+print (basket)
+print(len(basket))
+print (basket.split())
