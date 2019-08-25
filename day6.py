2bfcb8f3e30c20f30e005dd6d97357626cd078dd
mutebalotaibi <mzmz_222@hotmail.com>
2019-08-25 17:35:59 +0300

    week2- day6

---
+++ day6.py
@@ -0,0 +1,30 @@
+# week 2 
+# day 6 : python casting 
+
+# convert type  to integer from flat , string and aother
+x=int(1)      # x will be 1
+y=int(2.8)    # y will bd 2
+z=int('3')  # z will bd 3
+print(x)
+print(y)
+print(z)
+####################################################
+
+# converet to float type from integer , string and aother
+x=float(1)   # x will be 1.0
+y=float(2.8) # y will be 2.8
+z=float('3') # z will be 3.0
+w=float('4.2') #w will be 4.2
+print(x)
+print(y)
+print(z)
+print(w)
+
+#######################################################
+# converet to string type from integer , float and aother
+x=str("s1") # x will be 's1'
+y=str(2)    # y will be '2'
+z=str(3.0)  # zwill be '3.0'
+print(x)
+print(y)
+print(z)
