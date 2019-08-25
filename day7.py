8c17b1430083500de8538ee13d6fbfa6887b96ff
mutebalotaibi <mzmz_222@hotmail.com>
2019-08-25 18:33:37 +0300

    day7 task- python strings

---
+++ day7.py
@@ -0,0 +1,26 @@
+#week2
+#day7
+print("hello")
+print('hello')
+
+
+# assign string to a variable
+a="hello saudi development"
+print(a)
+###################################
+# assign multistring to a variable by using three quotes
+a=''' im learning a python programming language here tp develop my self and gitting a good knowledge ,
+we are finished the first week and now in the second week from the course'''
+print(a)
+
+# strings in python are arrays  
+a="hello, world"
+print(a[1])     # يتم تخزين النصوص في البايثون كمصفوفة وويبدأ ترقيم المصفوفة من 0
+
+###################################################
+#Substring. Get the characters from position 2 to position 5 (not included)
+#نقوم بتقسيم النص وتجزيئه من خلال العمل على اخذ عناصر المصفوفة من (2-5)
+a="hello, world"
+print(a[2:5]) # نقوم باخذ العناصر من 2 وحتى ماقبل 5
+
+######################################
