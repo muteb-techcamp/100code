e7afc704b14b3455a261df9247285fd08cecb84f
mutebalotaibi <mzmz_222@hotmail.com>
2019-08-27 18:00:23 +0300

    task 8 from week2

---
+++ day8.py
@@ -0,0 +1,25 @@
+#week2   ____  dsy8
+# السلاسل النصية # لحذف المسافات في بداية ونهايةالسلاسل النصية  نستخدم الدالة strip()
+a=" hello word! "
+print (a.strip())
+#####################################
+# to learn the length  of string we can use len() function
+a =" hello word! "
+print (len(a))
+####################################
+ #to print the string in lower case we are use lower()
+##############################################
+ #to print the string in lower case we are use lower()
+b =" muteb alotaibi "
+print(b.strip())
+print(b.upper())
+print(b.lower())
+print(b.title())
+ ########################################
+ #to replace string  we are use the function replace()
+s = " welcom in saudi arabia "
+print(s.replace("in","to"))
+##########################################
+#to split and devidied  a strings we can use split()
+w="hello , world!"
+print(w.split(","))
