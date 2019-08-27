b008cb9d5993d85952fa9daf84642c83eb918db2
mutebalotaibi <mzmz_222@hotmail.com>
2019-08-27 18:01:22 +0300

    task 9 from week2

---
+++ day9.py
@@ -0,0 +1,18 @@
+#week 2  ___ day9 
+#learn how can we use format() method to  combine strings and numbers
+# للدمج بين النصوص والارقام فإننا نستخدم دالة format
+age = 34
+txt= "my name is muteb, and i have{}"
+print(txt.format(age))
+
+###########################################
+# يمكن تمرير عدد غير محدود من  argument  وذلك لوضعها في مكان {}
+quantity=3
+itemno=567
+price=49.50
+myorder="i want {} pieces of item {} for {} dollars"
+print(myorder.format(quantity,itemno,price))
+######################################################
+first= "muteb" ; last= "alotaibi"
+full=f"{first} {last}"
+print(full)
