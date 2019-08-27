b3e19b50b7833d7579be417c32d8295b9b210eaa
mutebalotaibi <mzmz_222@hotmail.com>
2019-08-27 18:01:55 +0300

    task 10 from week2

---
+++ day10.py
@@ -0,0 +1,26 @@
+#week 2 --- day10
+#python operators
+x=5 ;y=6
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x/2)
+#######################
+x=10 
+x+=2
+print(x)
+x=5 
+x**=2
+print(x)
+x=15
+x^=2
+print(x)
+##################################
+# python comparison operators
+#using to comparing bertoween two variable and the resuls is true or false
+x=4 ;y=6
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x<=y)
