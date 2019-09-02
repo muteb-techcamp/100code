223974d919b9479ac7d41cead65b999f78ce1b72
mutebalotaibi <mzmz_222@hotmail.com>
2019-09-02 20:29:17 +0300

    task for day13 in week3

---
+++ day13.py
@@ -0,0 +1,33 @@
+# week 3 __  day13
+#to make a list 
+ 
+my_list = [100,200,300]
+print(my_list)
+#===============================
+name_of_student =['muteb', "ahmed", "trurki"]
+print(name_of_student)
+print(name_of_student[1])
+#================================
+city_of_ksa =['riyadh',"makkah","jeddah","dammam","jazan"]
+for n in city_of_ksa:
+     print(n)
+#===============================
+#create a list for a decimal number:
+my_list =[11.2 ,20.4 , 33.5 , 44.6]
+
+for x in my_list:
+    print(x)
+#===================================
+#change a mumber of from the_list :
+the_list=["apple","banana","cherry"]
+print(the_list)
+the_list[1]="orange"
+print(the_list)
+del the_list[2]
+print(the_list)
+
+list_no=[12,15,20,25,30]
+print(list_no)
+del list_no[0]
+del list_no[1]
+print(list_no)
