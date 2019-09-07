633260ff183e392a92aa24a9cc7251d49fe17c68
mutebalotaibi <mzmz_222@hotmail.com>
2019-09-07 13:07:53 +0300

    task 14 from week3

---
+++ day14.py
@@ -0,0 +1,24 @@
+#week3 ---day14
+#divided the list  تقسيم القوائم الى اجزاء
+mylist =["muteb","fahad","ahmed","sultam"]
+print(mylist[1:3])
+#================================
+# tn check if an elemnt of list exesit للتحقق من وجود عنصر في القوائم 
+fruite_list=["apple","banana","orange","cherry"]
+if  "apple" in fruite_list:
+ print(" yes, 'apple' in my best fruite list")
+#=================================
+#to repeate a vlue we can do :
+thislist=[" انا متعب"]*4
+print(thislist)
+ #=================================
+#to add more one list we are use + opreator
+thislist1=["muteb","age","gendere"]
+thislist2=["alotaibi",33,"male"]
+thislist3=thislist1+thislist2
+print(thislist3)
+#===============================
+list_of1=[10,12,14,25,28]
+list_of2=[11.50,41.55,15.82]
+list_of3=list_of1+list_of2
+print(list_of3)
