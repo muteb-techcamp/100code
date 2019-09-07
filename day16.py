8a512f92c60dd738cceedc6dbe18c146260bef4b
mutebalotaibi <mzmz_222@hotmail.com>
2019-09-07 13:11:41 +0300

    task 16 from week3

---
+++ day16.py
@@ -0,0 +1,24 @@
+#week 3 __  day16 
+#Create a Tuple in  python :
+thistuple =("ahmed","fahad","nowaf","naif")
+print(thistuple)
+thistuple1=()
+print(thistuple1)
+thistuple1=(4)
+print(thistuple1)
+thistuple2 =(20,33,15,15.2,40)
+print(thistuple2)
+print(thistuple2[2])
+for x in thistuple2:
+    print(x)
+
+##################################
+thistuple3 =("ahmed","fahad","nowaf","naif")
+#thistuple3[2]="yasser"
+#del thistuple3
+print(thistuple3) # tuple doesn't exeist when we execute the commend delthistuple3
+#**********************************
+thistuple3 =("ahmed","fahad","nowaf","naif")
+print(thistuple3[0:3])
+
+# لاتنسى لايمكنك اضافة عنصر لل tuple  وكذلك لايمكنك الحذف منه 
